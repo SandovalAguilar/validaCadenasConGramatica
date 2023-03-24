86a0341cb4440fb02e90e0643dd878fe9c8b63a7
SandovalAguilar <ismael.sandoval.aguilar@gmail.com>
2023-03-23 19:04:58 -0600

    cambios

--- validaGramatica.py
+++ validaGramatica.py
@@ -18,7 +18,8 @@ matriculaPredeterminada = "1943548"
 '''
 
 # Variables Globales de prueba
-nombrePredeterminado = "Yozedh Jahday Guerrero Ceja"
+nombresPredeterminados = "Yozedh Jahday"
+apellidosPredeterminados = "Guerrero Ceja"
 matriculaPredeterminada = "0123456"
 
 # Funciones para generar conjuntos e iniciales
@@ -31,22 +32,13 @@ def generaIniciales(apellidos):
     
     return inicialesApellidos
 
-def generaDigitos(matricula):
-    matriculaConjunto = list(set(matricula))
-    digitos = ''
-
-    for i in matriculaConjunto:
-        digitos += (i)
-
-    return digitos
-
-def inicialesInversa(inicialesApellidos):
+def generaInicialesInversa(inicialesApellidos):
     return inicialesApellidos[::-1]
 
-def primerNombre(nombreCompleto):
+def encuentraPrimerNombre(nombreCompleto):
     return nombreCompleto.split(' ')[0]
 
-def analizaCadenas(inicialesApellidos, inicialesInversa, matricula, primerNombre):
+def validaCadenas(inicialesApellidos, inicialesInversa, matricula, primerNombre):
     patron = re.compile('\A(' + matricula + ')(' + inicialesApellidos + ')+(' + matricula + ')((' + inicialesInversa + '){2})+((' + primerNombre * 2 + ')\Z)')    
     patrongm = re.compile('\A(' + matricula + ')(' + inicialesApellidos + ')+(' + matricula + ')')
     patronmg = re.compile('(' + matricula + ')((' + inicialesInversa + '){2})*((' + primerNombre * 2 + ')\Z)')
@@ -55,33 +47,31 @@ def analizaCadenas(inicialesApellidos, inicialesInversa, matricula, primerNombre
     encols= str(patrongm.match(cadena))
     encosl= str(patronmg.search(cadena))
 
-    contarls= encols.count('gc')
-    contarsl= encosl.count("cg")
+    contarls= encols.count(inicialesApellidos)
+    contarsl= encosl.count(inicialesInversa)
 
     if (contarls >= 1) and (encocadena != None) and ( contarsl >= 1) and (contarls*2 == contarsl):
         print("\nCadena válida")
     else:
         print("\nCádena inválida")
 
-# Por hacer: comprobar apellidos
-
-def imprimeConjuntos(nombre, matricula, apellidos):
-    print("--------------------------------------------------")
-    print("Nombre ingresado: " + nombre)
-    print("Matricula ingresada: " + matricula)
-    print("Conjuntos generados a partir del nombre y matricula:")
-    print("w = " + generaIniciales(apellidos))
-    print("i = " + generaDigitos(matricula))
-    print("w^i = " + inicialesInversa(generaIniciales(apellidos)))
-    print("j = " + primerNombre(nombre))
-    print("--------------------------------------------------")
-
 # Programa principal
 def main():
-    analizaCadenas(inicialesApellidos= 'gc',
-                   inicialesInversa = 'cg',
-                   matricula = '0123456',
-                   primerNombre = 'yozedh')
+    
+    inicialesApellidos = generaIniciales(apellidosPredeterminados)
+    inicialesInversa = generaInicialesInversa(inicialesApellidos)
+    matricula = matriculaPredeterminada
+    primerNombre = encuentraPrimerNombre(nombresPredeterminados).lower()
+
+    print(inicialesApellidos,
+          inicialesInversa,
+          matricula,
+          primerNombre)
+ 
+    validaCadenas(inicialesApellidos,
+                   inicialesInversa,
+                   matricula,
+                   primerNombre)
 
 if __name__=="__main__":
     main()
